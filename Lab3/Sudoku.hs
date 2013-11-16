8e05f7956d23a5ad41d3ba00fbf290564546ce30
Jesper Persson <jespert.persson@gmail.com>
2013-11-16 19:12:34 +0100

    Implement function isSudoku

--- Lab3/Sudoku.hs
+++ Lab3/Sudoku.hs
@@ -1,9 +1,68 @@
-data Sudoku = Sudoku {rows :: [[Maybe Int]] }
-    deriving Show
+module Sudoku where
 
+import Test.QuickCheck
+import Data.Maybe
+
+-------------------------------------------------------------------------
+
+data Sudoku = Sudoku { rows :: [[Maybe Int]] }
+ deriving ( Show, Eq )
+
+example :: Sudoku
+example =
+    Sudoku
+      [ [Just 3, Just 6, Nothing,Nothing,Just 7, Just 1, Just 2, Nothing,Nothing]
+      , [Nothing,Just 5, Nothing,Nothing,Nothing,Nothing,Just 1, Just 8, Nothing]
+      , [Nothing,Nothing,Just 9, Just 2, Nothing,Just 4, Just 7, Nothing,Nothing]
+      , [Nothing,Nothing,Nothing,Nothing,Just 1, Just 3, Nothing,Just 2, Just 8]
+      , [Just 4, Nothing,Nothing,Just 5, Nothing,Just 2, Nothing,Nothing,Just 9]
+      , [Just 2, Just 7, Nothing,Just 4, Just 6, Nothing,Nothing,Nothing,Nothing]
+      , [Nothing,Nothing,Just 5, Just 3, Nothing,Just 8, Just 9, Nothing,Nothing]
+      , [Nothing,Just 8, Just 3, Nothing,Nothing,Nothing,Nothing,Just 6, Nothing]
+      , [Nothing,Nothing,Just 7, Just 6, Just 1, Nothing,Nothing,Just 4, Just 3]
+      ]
+
+-- allBlankSudoku is a sudoku with just blanks
 allBlankSudoku :: Sudoku
 allBlankSudoku = Sudoku (makeBlankSudoku 9 [])
     where makeBlankSudoku 0 m = m
-          makeBlankSudoku n m = makeBlankSudoku (n-1) ((makeBlankRow 9 []):m)
-                where makeBlankRow 0 xs = xs
-                      makeBlankRow n xs = makeBlankRow (n-1) (Nothing:xs)
+          makeBlankSudoku n m = makeBlankSudoku (n-1) ((replicate 9 Nothing):m)
+ 
+-- isSudoku sud checks if sud is really a valid representation of a sudoku
+-- puzzle
+
+isSudoku :: Sudoku -> Bool
+isSudoku sudoku 
+    | length (rows sudoku) /= 9             = False
+    | otherwise                             = and [check row | row <- rows sudoku]
+        where check row | length row /= 9   = False 
+                        | otherwise         = and [(fromMaybe 1 y) `elem` [1..9] | y <- row]
+
+-- isSolved sud checks if sud is already solved, i.e. there are no blanks
+isSolved :: Sudoku -> Bool
+isSolved = undefined
+
+-------------------------------------------------------------------------
+
+-- printSudoku sud prints a representation of the sudoku sud on the screen
+printSudoku :: Sudoku -> IO ()
+printSudoku = undefined
+
+-- readSudoku file reads from the file, and either delivers it, or stops
+-- if the file did not contain a sudoku
+readSudoku :: FilePath -> IO Sudoku
+readSudoku = undefined
+
+-------------------------------------------------------------------------
+
+-- cell generates an arbitrary cell in a Sudoku
+cell :: Gen (Maybe Int)
+cell = undefined
+
+-- an instance for generating Arbitrary Sudokus
+instance Arbitrary Sudoku where
+  arbitrary =
+    do rows <- sequence [ sequence [ cell | j <- [1..9] ] | i <- [1..9] ]
+       return (Sudoku rows)
+
+-------------------------------------------------------------------------
